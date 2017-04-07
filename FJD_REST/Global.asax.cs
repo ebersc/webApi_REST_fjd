5dcf14d17ccae5929cec573e50ab1c57c1e5c7e8
Eber <ebersc@users.noreply.github.com>
2017-04-07 10:01:46 -0300

    Update Global.asax.cs

--- FJD_REST/Global.asax.cs
+++ FJD_REST/Global.asax.cs
@@ -14,11 +14,15 @@ namespace FJD_REST
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            
+            //use {servidor}/{api}/{controller}/?{op}&type=json para que o metodo retorne automaticamente um JSON
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
-    new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
-
+                new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+            //use {servidor}/{api}/{controller}/?{op}&type=xml para que o metodo retorne automaticamente um XML
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(
                 new QueryStringMapping("type", "xml", new MediaTypeHeaderValue("application/xml")));
+              
+                //Caso o parametro TYPE nao seja informado automaticamente a WebApi gerara um XML como retorno
         }
     }
 }
